09d5145d74204753426afc1c6bdeddd08659d6a4
mauroapadula-beep <mauro.a.padula@gmail.com>
2026-05-13 16:34:34 -0300

    Create page.js

---
+++ app/shopping/page.js
@@ -0,0 +1,9 @@
+export default function ShoppingPage() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'Arial' }}>
+      <h1>🛒 Shopping List</h1>
+
+      <p>Your automatic shopping list will appear here.</p>
+    </div>
+  )
+}
