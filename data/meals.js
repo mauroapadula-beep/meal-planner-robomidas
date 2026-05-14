fac5d12264945692bfb970a0f0777a937010576a
mauroapadula-beep <mauro.a.padula@gmail.com>
2026-05-14 17:26:30 -0300

    Create meals.js

---
+++ data/meals.js
@@ -0,0 +1,88 @@
+export const weeklyPlan = [
+  {
+    day: 'Monday',
+    lunch: 'Grilled Chicken + Rice',
+    dinner: 'Pumpkin Soup',
+  },
+  {
+    day: 'Tuesday',
+    lunch: 'Fish + Vegetables',
+    dinner: 'Pasta Bolognese',
+  },
+  {
+    day: 'Wednesday',
+    lunch: 'Beef + Sweet Potato',
+    dinner: 'Lentil Stew',
+  },
+  {
+    day: 'Thursday',
+    lunch: 'Chicken Salad',
+    dinner: 'Vegetable Omelette',
+  },
+  {
+    day: 'Friday',
+    lunch: 'Rice Bowl',
+    dinner: 'Homemade Pizza',
+  },
+]
+
+export const ingredientsMap = {
+  'Grilled Chicken + Rice': [
+    'Chicken',
+    'Rice',
+    'Olive Oil',
+  ],
+
+  'Pumpkin Soup': [
+    'Pumpkin',
+    'Onion',
+    'Cream',
+  ],
+
+  'Fish + Vegetables': [
+    'Fish',
+    'Broccoli',
+    'Carrot',
+  ],
+
+  'Pasta Bolognese': [
+    'Pasta',
+    'Tomato',
+    'Ground Beef',
+  ],
+
+  'Beef + Sweet Potato': [
+    'Beef',
+    'Sweet Potato',
+  ],
+
+  'Lentil Stew': [
+    'Lentils',
+    'Carrot',
+    'Onion',
+  ],
+
+  'Chicken Salad': [
+    'Chicken',
+    'Lettuce',
+    'Tomato',
+  ],
+
+  'Vegetable Omelette': [
+    'Eggs',
+    'Spinach',
+    'Cheese',
+  ],
+
+  'Rice Bowl': [
+    'Rice',
+    'Eggs',
+    'Avocado',
+  ],
+
+  'Homemade Pizza': [
+    'Flour',
+    'Cheese',
+    'Tomato Sauce',
+  ],
+}
